027f17a85f0679a92d77bb1ab4a1633ce548cad8
Xanadu-zs <zs_xanadu@163.com>
2018-09-03 10:44:00 +0800

    笑话生成器

--- MDN--JS/JS--第一弹/蠢萌笑话生成器.js
+++ MDN--JS/JS--第一弹/蠢萌笑话生成器.js
@@ -1,46 +1,47 @@
-1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
-
 var customName = document.getElementById('customname');
 var randomize = document.querySelector('.randomize');
 var story = document.querySelector('.story');
 
-function randomValueFromArray(array){
-  return array[Math.floor(Math.random()*array.length)];
+function randomValueFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
 }
 
-2. RAW TEXT STRINGS
 
-It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+var storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+var insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
 
-Willy the Goblin
-Big Daddy
-Father Christmas
+var insertY = ['the soup kitchen','Disneyland','the White House'];
 
-the soup kitchen
-Disneyland
-the White House
+var insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
 
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
-
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
 function result() {
+  var newStory = storyText;
+  var xItem = randomValueFromArray(insertX);
+  var yItem = randomValueFromArray(insertY);
+  var zItem = randomValueFromArray(insertZ);
 
-  if(customName.value != '') {
-    var name = customName.value;
+  newStory = newStory.replace(':insertx:',xItem);
+  newStory = newStory.replace(':inserty:',yItem);
+  newStory = newStory.replace(':insertz:',zItem);
 
-  }
 
-  if(document.getElementById("uk").checked) {
-    var weight = Math.round(300);
-    var temperature =  Math.round(94);
+  
 
-  }
+    if (customName.value != '') {
+        var name = customName.value;
+        newStory = newStory.replace('Bob',name);
+    }
 
-  story.textContent = ;
-  story.style.visibility = 'visible';
+    if (document.getElementById("uk").checked) {
+        var weight = Math.round(300*0.0714286) +' stone';
+        var temperature = Math.round(94-32) * 5 / 9 +' centigrade';
+        var newStory = newStory.replace('94 farenheit',temperature);
+        var newStory = newStory.replace('300 pounds',weight);
+    }
+
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
 }
