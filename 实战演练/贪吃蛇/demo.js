0b897c5faeb9aa7d7d71c3beea80a329050d0404
Xanadu-zs <zs_xanadu@163.com>
2018-12-02 16:56:40 +0800

    贪吃蛇

---
+++ 实战演练/贪吃蛇/demo.js
@@ -0,0 +1,59 @@
+//点击开始游戏>>startPage消失   游戏开始
+//随机出现食物,出现三界狂蛇开始运动
+//上下左右>>改变方向
+//判断是否吃到食物>>食物消失,狂蛇加一
+//判段游戏结束弹出框
+//
+
+var content = document.getElementById('content');
+var startPage = document.getElementById('startPage');
+
+init();
+function init() {
+    //地图
+    this.mapW = parseInt(window.getComputedStyle(content).width);
+    //取得宽高
+    this.mapH = parseInt(window.getComputedStyle(content).height);
+    this.mapDiv = content;
+    //食物
+    this.foodW = 20;
+    this.foodH = 20;
+    this.foodX = 0;
+    this.foodY = 0;
+    // 蛇
+    this.snakeW = 20;
+    this.snakeH = 20;
+    this.snakeBody = [[3,1,'head'],[2,1,'body'],[1,1,'body']];
+    startGame();
+}
+
+function startGame() {
+    food();
+    snake();
+}
+
+function food() {
+    var food = document.createElement('div');
+    food.style.width = this.foodW + 'px';
+    food.style.height = this.foodH + 'px';
+    food.style.borderRadius = '50%';
+    this.foodX = Math.floor(Math.random() * (this.mapW / this.foodW));
+    this.foodY = Math.floor(Math.random() * (this.mapH / this.foodH));
+    
+    food.style.left = this.foodX * this.foodW + 'px';
+    food.style.top = this.foodY * this.foodH + 'px';
+
+    food.style.position = 'absolute';
+    this.mapDiv.appendChild(food).setAttribute('class', 'food');
+}
+
+function snake() {
+    for (var i = 0; i < snakeBody.length; i++) {
+    var snake = document.createElement('div');   
+    snake.style.width = this.snakeW + 'px';
+    snake.style.height = this.snakeH + 'px';
+    snake.style.position = 'absolute';
+    snake.style.left = this.snakeBody[i][0] *20 +'px';
+    snake.style.top = this.snakeBody[i][1] *20 +'px';
+    }
+}
