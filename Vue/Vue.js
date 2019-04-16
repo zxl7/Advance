29d2c7e81f082e4b911ba684081a740325d6c9b4
Xanadu-zs <zs_xanadu@163.com>
2019-04-16 22:22:14 +0800

    vue学习

--- Vue/Vue.js
+++ Vue/Vue.js
@@ -8539,10 +8539,10 @@ if (inBrowser) {
         "development" !== 'test' &&
         isChrome
       ) {
-        console[console.info ? 'info' : 'log'](
-          'Download the Vue Devtools extension for a better development experience:\n' +
-          'https://github.com/vuejs/vue-devtools'
-        );
+        // console[console.info ? 'info' : 'log'](
+        //   'Download the Vue Devtools extension for a better development experience:\n' +
+        //   'https://github.com/vuejs/vue-devtools'
+        // );
       }
     }
     if ("development" !== 'production' &&
@@ -8550,11 +8550,11 @@ if (inBrowser) {
       config.productionTip !== false &&
       typeof console !== 'undefined'
     ) {
-      console[console.info ? 'info' : 'log'](
-        "You are running Vue in development mode.\n" +
-        "Make sure to turn on production mode when deploying for production.\n" +
-        "See more tips at https://vuejs.org/guide/deployment.html"
-      );
+      // console[console.info ? 'info' : 'log'](
+      //   "You are running Vue in development mode.\n" +
+      //   "Make sure to turn on production mode when deploying for production.\n" +
+      //   "See more tips at https://vuejs.org/guide/deployment.html"
+      // );
     }
   }, 0);
 }
