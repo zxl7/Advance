40dde17bbc68bedcf6251b2bff5b9b2b6c51645e
Xanadu-zs <zs_xanadu@163.com>
2019-04-22 22:31:22 +0800

    闭包

---
+++ JavaScript高级程序设计/乘坐头等舱.js
@@ -0,0 +1,31 @@
+var passengers = [{ name: 'bob', paid: ture }, { name: 'bob', paid: ture }, { name: 'bob', paid: ture }, { name: 'bob', paid: ture }];
+
+
+function createDrink(passenger) {
+    var orderFunction;
+    if (passengers.ticket === "firstlclass") {
+        orderFunction = function() {
+            alert('apple and wine?');
+        } else {
+            orderFunction = function() {
+                alert('apple and wine?');
+            }
+        }
+    }
+}
+
+function create (passengers) {
+	var orderFunction;
+	if (passengers.ticket === "firstlclass") {
+	 	orderFunction = function () {
+	 		alert("toudengcang ");
+	 	}else if(passengers.ticket === "premium"){
+	 		orderFunction = function () {
+	 		alert("hanghua ");;
+	 	}else {
+	 		orderFunction = function () {
+	 		alert("jinjicang ");
+	 	}
+	 } 
+
+}
--- 实战演练/新闻发布系统/中国新闻网_梳理天下新闻_files/p.jsp
+++
@@ -1,6 +0,0 @@
-
-
-
-
-var cnsservce_c='ok';
-
