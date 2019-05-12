98c133fd06ac3a2e9bae6200622c53e44ba95fad
Xanadu-zs <zs_xanadu@163.com>
2019-05-12 20:55:58 +0800

    Vue 又来了

--- Vue/my-project1/src/main.js
+++ Vue/my-project1/src/main.js
@@ -3,13 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import store from './store/'
-import VueResource from 'vue-resource'
-// import MintUI from 'mint-ui'
-// import 'mint-ui/lib/style.css'
-
-Vue.use(VueResource)
-// Vue.use(MintUI)
 
 Vue.config.productionTip = false
 
@@ -17,7 +10,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  store,
   components: { App },
   template: '<App/>'
 })
