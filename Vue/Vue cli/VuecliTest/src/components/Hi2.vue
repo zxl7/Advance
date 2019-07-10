62d5310f63148bc306090bd22427edadcbb3b8e1
Xanadu-zs <zs_xanadu@163.com>
2019-07-10 14:13:25 +0800

    Vue cli  finish

---
+++ Vue/Vue cli/VuecliTest/src/components/Hi2.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hi2",
+  data() {
+    return {
+      msg: "新的学习Hi2"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
