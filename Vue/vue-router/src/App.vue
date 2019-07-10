b8ae4efe218c969f8f91b16e7d3e6e52258bfc6d
Xanadu-zs <zs_xanadu@163.com>
2019-07-10 14:49:14 +0800

    Vue-router init

---
+++ Vue/vue-router/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <router-view />
+    <router-view name="left" style="float:left;width:50%;background-color:#ccc;height:300px;" />
+    <router-view name="right" style="float:right;width:50%;background-color:#c0c;height:300px;" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue/vue-router/src/components/Hi1.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>{{msg}}</h2>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg:'I am H111111!'
+    }
+  }
+}
+</script>
