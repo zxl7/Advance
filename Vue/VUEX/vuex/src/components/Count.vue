c68c4ffbe1210beec8c955433fba7ab5b8034155
Xanadu-zs <zs_xanadu@163.com>
2019-07-13 23:00:45 +0800

    VueX

---
+++ Vue/VUEX/vuex/src/components/Count.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+    <h3>{{$store.state.count}}</h3>
+  </div>
+</template>
+
+<script>
+import store from '@vuex/store'
+export default {
+  data () {
+    return {
+      msg: 'hello Vuex'
+    }
+  },
+  store
+}
+</script>
