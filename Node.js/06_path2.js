2d4e7e257ab9fb5c0f6754bb96355774da1de149
xanadu-zxl <xanadu.zxl@gmail.com>
2019-08-05 14:28:33 +0800

    Node、js 第一天

---
+++ Node.js/06_path2.js
@@ -0,0 +1,14 @@
+// 接受一段字符串路径
+const path = require('path');
+let myPath = path.join(__dirname,'jack','rose','mick.txt');
+
+// 解析这个路径为对象，更易于操作
+let pathObj = path.parse(myPath);
+// console.log(pathObj);
+
+// base可以作为修改文件名，或后缀的方式
+pathObj.base = 'mick_die.good';
+
+// 接收路径对象，转换成路径字符串
+myPath = path.format(pathObj);
+console.log(myPath);
