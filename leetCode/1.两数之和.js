31be437496b85b710795d5b6a0f2b4dcebac7569
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-08-07 22:36:57 +0800

    leetcode

--- leetCode/1.两数之和.js
+++ leetCode/1.两数之和.js
@@ -8,17 +8,13 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  var ret = []
-  var exist = {}
-  for (var i = 0; i < nums.length; i++) {
-    if (typeof exist[target - nums[i]] !== 'undefined') {
-      ret.push(exist[target - nums[i]])
-      ret.push(i + 1)
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j]
+      }
     }
-
-    exist[nums[i]] = i + 1
   }
-
-  return ret
 }
+
