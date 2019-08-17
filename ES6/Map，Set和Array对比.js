f5c7dc7389b13110ee9906f5dfe07e3ab99cdb5f
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-08-17 10:46:23 +0800

    map，set，Array对比

--- ES6/Map，Set和Array对比.js
+++ ES6/Map，Set和Array对比.js
@@ -19,5 +19,32 @@
   map.delete('t')
   let index = array.findIndex(item => item.t)
   array.splice(index, 1)
+}
 
+
+{
+  let set = new Set()
+
+  // 增
+  set.add({ t: 1 })
+
+  // 查
+  let set_exist = set.has({ t: 1 })
+  // 改
+  set.forEach(item => item.t ? item.t = 2 : '')
+
+  // 删
+  set.forEach(item => item.t ? set.delete(item) : '')
+}
+
+// objedct
+{
+  let obj = {}
+  obj['t'] = 1
+
+  obj_exist: 't' in obj
+
+  obj['t'] = 2
+
+  delete obj['t']
 }
