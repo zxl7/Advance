8007fc94bac5e4e1069e177d12f7e44179df97fe
Xanadu丶zxl <xanadu.zxl@gmail.com>
2019-08-27 22:18:58 +0800

    Update 5. 观察者模式.js

--- JavaScript设计模式/5. 观察者模式.js
+++ JavaScript设计模式/5. 观察者模式.js
@@ -1,3 +1,40 @@
+// 主题，保存状态，状态变化后触发所有观察者模式
 class Subject {
-  constructor() {}
+  constructor() {
+    this.state = 0
+    this.observers = []
+  }
+  getState() {
+    return this.state
+  }
+  setState() {
+    this.state = state
+    this.notifyAllObservers()
+  }
+  notifyAllObservers() {
+    this.observers.forEach(observers => {
+      observers.update()
+    })
+  }
+  attch(observer) {
+    this.observers.push(observer)
+  }
 }
+
+// 观察者
+class Observer {
+  constructor(name, subject) {
+    this.name = name
+    this.subject = subject
+    this.subject.attach(this)
+
+  }
+  update() {
+    console.log('${this.name} update,state:${this.subject.getState}')
+  }
+}
+
+let s = new Subject()
+let o1 = new Observer('o1', s)
+
+s.setState(1)
