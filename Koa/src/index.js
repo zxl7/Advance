ecde553e3200172b49f82bd9291aaf9ab1023d7a
xanadu-zxl <xanadu.zxl@gamil.com>
2020-11-07 22:55:35 +0800

    feat：koa静态资源

--- Koa/src/index.js
+++ Koa/src/index.js
@@ -1,9 +1,11 @@
 const koa = require('koa')
+const statics = require('koa-statics')
 
 const app = new koa()
 
 const router = require('./routes/routes')
 
 app.use(router())
+app.use(statics(path.join(__dirname, '../pubilc')))
 
 app.listen(3000)
