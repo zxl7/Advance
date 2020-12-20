86cd987335f7c9208a3a20fbbb981c79391f79dd
xanadu-zxl <xanadu.zxl@gamil.com>
2020-12-20 23:42:19 +0800

    💄 nice

--- more-demo/src/views/Home.vue
+++ more-demo/src/views/Home.vue
@@ -2,6 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>个人作品集</h1>
+    <main class="main">
+      <div class="rounds">
+        <div class="round1"></div>
+        <div class="round2"></div>
+        <div class="round3"></div>
+        <div class="round4"></div>
+        <div class="round5"></div>
+        <div class="round6"></div>
+        <div class="round7"></div>
+        <div class="round8"></div>
+        <div class="round9"></div>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -13,3 +26,54 @@ import { Options, Vue } from 'vue-class-component'
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+.main {
+  height: 400px;
+  width: 400px;
+  margin: 0 auto;
+  border-radius: 6px;
+  background: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  filter: contrast(30);
+
+  .rounds {
+    position: relative;
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    background: #fff;
+    filter: blur(6px);
+
+    .round1 {
+      filter: blur(6px);
+      position: absolute;
+      width: 26px;
+      height: 26px;
+      top: 50%;
+      left: 50%;
+      transform: translateX(-50%) translateY(-50%);
+      background-color: #fff;
+      border-radius: 50%;
+      animation: inside-motion 3s linear infinite;
+      transform-origin: 40px 0px;
+    }
+
+    .round2 {
+      @extend .round1;
+      transform-origin: 40px 40px;
+    }
+
+    @keyframes inside-motion {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  }
+}
+</style>
