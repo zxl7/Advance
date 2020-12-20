a9e64f090b9814c7dd2e5ddab2fe148f28e04bde
xanadu-zxl <xanadu.zxl@gamil.com>
2020-12-20 22:28:45 +0800

    ✨ init

--- more-demo/src/views/Home.vue
+++ more-demo/src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>个人作品集</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Options, Vue } from 'vue-class-component'
 
 @Options({
-  components: {
-    HelloWorld
-  }
+  components: {},
 })
 export default class Home extends Vue {}
 </script>
--- more-demo/vue.config.js
+++ more-demo/vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/advance/' : '/',
   lintOnSave: false,
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'SpecialEffects'
+      return args
+    })
+  },
 }
