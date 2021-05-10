4931b411abaeda5992a2d28c79a056dc4be7489f
xanadu-zxl <xanadu.zxl@gamil.com>
2021-05-10 12:00:58 +0800

    feat：react test

---
+++ React/my-app/src/Test.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css';
+
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>111</div>
+      </div>
+    )
+  }
+}
+
+export default Test
