2287ab06bf0d800718751c7b0b06902807145117
xanadu-zxl <xanadu.zxl@gamil.com>
2021-06-20 15:47:15 +0800

    feat：react  init

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Test from './Test';
-import reportWebVitals from './reportWebVitals';
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Test />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals();
+  document.getElementById('root'),
+)
